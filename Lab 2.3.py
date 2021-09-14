ed10c958a9ed22ddbba1cc1c93b17d207a68b81b
Long-Choon-Heng <90229489+Long-Choon-Heng@users.noreply.github.com>
2021-09-14 10:41:31 +0800

    Add files via upload

---
+++ Lab 2.3.py
@@ -0,0 +1,9 @@
+#Student ID: 1201200670
+#Student Name: Long Choon Heng
+
+temperature = float(input("Please enter your temperature"))
+temperature = temperature * (0°C × 9/5) + 32 = 32°F
+
+#print(temperature,"°C is equivalent to",°F,"°F")
+print("{:2f}°C is equivalent to {:2f} °F".format(°C,°F))
+
